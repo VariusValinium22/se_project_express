65a5d40a9981225b0c95f910cd746ed9df338349
Martin Young <MartingYoung@hotmail.com>
2024-12-24 09:07:51 -0600

    finished adding Controllers. Having an issue getting Postman request results to appear in MongoDB Compass

--- controllers/users.js
+++ controllers/users.js
@@ -2,7 +2,10 @@ const User = require("../models/user");
 
 const getUsers = (req, res) => {
   User.find({})
-    .then((users) => res.status(200).send(users))
+    .then((users) => {
+      /* console.log("All Users:", users); */
+      res.status(200).send(users);
+    })
     .catch((err) => {
       console.error(err);
       return res.status(500).send({ message: err.message });
@@ -13,7 +16,10 @@ const getUser  = (req, res) => {
   const { userId } = req.params;
   User.findById(userId)
     .orFail()
-    .then((user) => res.status(200).send(user))
+    .then((user) => {
+      /* console.log("Found User:", user); */
+      res.status(200).send(user);
+    })
     .catch((err) => {
       console.error(err);
       if (err.name === "DocumentNotFoundError") {
@@ -27,11 +33,12 @@ const getUser  = (req, res) => {
 
 const createUser = (req, res) => {
   const { name, avatar } = req.body;
-  console.log(req.body);
+  /* console.log(req.body); */
   User.create({ name, avatar })
     .then((user) => res.status(201).send(user))
     .catch((err) => {
       console.error(err);
+      console.log("Name of Error: ", err.name);
       if (err.name === "ValidationError") {
         return res.status(400).send({ message: err.message });
       }
@@ -39,4 +46,24 @@ const createUser = (req, res) => {
     });
 };
 
-module.exports = { getUsers, createUser, getUser };
+const deleteUser = (req,res) => {
+  const { userId } = req.params;
+  User.findByIdAndDelete(userId)
+    .orFail()
+    .then((user) => {
+      console.log("User has been DELETED:", user);
+      res.status(200).send({ message: "User has been deleted", user })
+    })
+    .catch((err) => {
+      console.error(err);
+      if (err.name === "DocumentNotFoundError") {
+        return res.status(404).send({ message: err.message });
+      } else if (err.name === "CastError") {
+        return res.status(400).send({ message: err.message });
+      }
+      return res.status(500).send({ message: err.message });
+    })
+};
+
+
+module.exports = { getUsers, getUser, createUser, deleteUser };
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,9 @@
 const router = require("express").Router();
-const { getUsers, getUser, createUser } = require("../controllers/users");
+const { getUsers, getUser, createUser, deleteUser } = require("../controllers/users");
 
 router.get("/", getUsers);
 router.get("/:userId", getUser);
 router.post("/", createUser);
+router.delete("/:userId", deleteUser);
 
 module.exports = router;
